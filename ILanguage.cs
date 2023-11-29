1787a5a892647bd482d93c6e6955c754930023df
Paweł Tobis - Softbery <softbery@gmail.com>
2023-11-29 10:44:59 +0100

    Create Translator and set translating interface

---
+++ ILanguage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator
+{
+    public interface ILanguage
+    {
+        string Name { get; }
+        string formSettings_btnSave { get; }
+        string formSettings_btnCancel { get; }
+        string formSettings_Text { get; }
+        string formSettings_labelOptions { get; }
+
+        #region VideoControlBar
+        string videoControlBar_labelSubtilesOnOffText { get; }
+        string videoControlBar_labelOn { get; }
+        string videoControlBar_labelOff { get; }
+        string videoControlBar_fullscreenOff { get; }
+        string videoControlBar_fullscreenOn { get; }
+        string videoControlBar_startTxt { get; }
+        string videoControlBar_subtilesOn { get; }
+        string videoControlBar_subtilesOff { get; }
+        #endregion
+    }
+}
---
+++ Language.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator
+{
+    public static class Language
+    {
+        private static IList<ILanguage> _langs = new List<ILanguage>();
+
+        private static string _path = "languages/";
+
+        public static string LanguageDir { get => _path; }
+
+        public static IList<ILanguage> LoadLanguage(string dir_path = null)
+        {
+            if (dir_path != null)
+            {
+                setLanguagesDir(dir_path);
+            }
+
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+
+            var files = Directory.GetFiles(_path, "*.dll");
+
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    var assembly = Assembly.LoadFrom(file);
+                    var types = assembly.GetExportedTypes();
+
+                    foreach (Type type in types)
+                    {
+                        if (type.GetInterfaces().Contains(typeof(ILanguage)))
+                        {
+                            var obj = Activator.CreateInstance(type);
+                            _langs.Add(obj as ILanguage);
+                        }
+                    }
+                }
+            }
+            return _langs;
+        }
+
+        private static void setLanguagesDir(string dir)
+        {
+            var dir_path = new DirectoryInfo(dir);
+            if (dir_path.Exists)
+            {
+                _path = dir_path.FullName;
+            }
+        }
+
+        public static object UseLanguage<T>(this T obj)
+        {
+            if (_langs.Contains((ILanguage)obj))
+                return (T)Activator.CreateInstance(obj.GetType());
+            else
+                return null;
+        }
+    }
+}
